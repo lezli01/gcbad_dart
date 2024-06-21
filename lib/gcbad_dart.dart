4d8049f0ed19d446da8edc9268f647152a3ddf8a
lezli01 <lezli01@gmail.com>
2024-06-21 20:53:49 +0200

    export models

--- lib/gcbad_dart.dart
+++ lib/gcbad_dart.dart
@@ -4,3 +4,20 @@
 library;
 
 export 'src/gocardless_bank_account_data_client.dart';
+
+export 'src/models/account.dart';
+export 'src/models/account_details.dart';
+export 'src/models/balance.dart';
+export 'src/models/balances.dart';
+export 'src/models/error_response.dart';
+export 'src/models/end_user_agreement.dart';
+export 'src/models/end_user_agreement_request.dart';
+export 'src/gocardless_country_code.dart';
+export 'src/models/information_to_access.dart';
+export 'src/models/institution.dart';
+export 'src/models/institution_metadata.dart';
+export 'src/models/requisition.dart';
+export 'src/models/requisition_request.dart';
+export 'src/models/secretandkey.dart';
+export 'src/models/token.dart';
+export 'src/models/transactions.dart';
