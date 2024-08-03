ebc9acb014ac64e66f6a896a4b30ea142ede9fbe
lezli01 <lezli01@gmail.com>
2024-08-03 21:50:18 +0200

    export gcbadexception

--- lib/gcbad_dart.dart
+++ lib/gcbad_dart.dart
@@ -4,6 +4,8 @@
 library;
 
 export 'src/gocardless_bank_account_data_client.dart';
+export 'src/gocardless_country_code.dart';
+export 'src/gocardless_exception.dart';
 
 export 'src/models/account.dart';
 export 'src/models/account_details.dart';
@@ -12,7 +14,6 @@ export 'src/models/balances.dart';
 export 'src/models/error_response.dart';
 export 'src/models/end_user_agreement.dart';
 export 'src/models/end_user_agreement_request.dart';
-export 'src/gocardless_country_code.dart';
 export 'src/models/information_to_access.dart';
 export 'src/models/institution.dart';
 export 'src/models/institution_metadata.dart';
